e8f97180d18c4157cdd0c52470fdfa37a28abb27
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-09-05 14:31:33 +0200

    fix: use py39 compatible Optional for Node.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,35 @@ def basic_lci_as_dict():
         "code": "a code",
         "database": "a database",
     }
+
+
+@pytest.fixture
+def bike_process():
+    return {"name": "bike production", "code": "bike", "location": "DK", "unit": "bike"}
+
+
+@pytest.fixture
+def ng_process():
+    return {
+        "name": "natural gas production",
+        "unit": "MJ",
+        "location": "NO",
+        "code": "ng",
+        "database": "bikes",
+    }
+
+
+@pytest.fixture
+def cf_process():
+    return {
+        "name": "carbon fibre production",
+        "unit": "kg",
+        "location": "DE",
+        "code": "cf",
+        "database": "bikes",
+    }
+
+
+@pytest.fixture
+def basic_node_as_dict():
+    return {"code": "simple code", "database": "les fleurs du mal"}
