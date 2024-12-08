75fd46bae14e267c0792603d4d54b3e799a22b5a
Chris Mutel <cmutel@gmail.com>
2024-12-08 15:29:15 +0100

    Fix test function names

--- tests/integration.py
+++ tests/integration.py
@@ -1,9 +1,9 @@
 from bw_interface_schemas import GraphLoader
 
 
-def dump_graph(bike_as_graph):
+def test_dump_graph(bike_as_graph):
     assert bike_as_graph.model_dump()
 
 
-def construct_graph(bike_as_dict):
+def test_construct_graph(bike_as_dict):
     assert GraphLoader(identifier_field="name").load(bike_as_dict, use_identifiers=True)
